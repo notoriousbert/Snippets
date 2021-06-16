cbd04b8db4fa68fc6666263559da35b483bb7c91
Albert Meuse <albertmeuse22@gmail.com>
2021-06-16 14:04:34 -0700

    created endpoints for /teapot, /add, and /hello, that are fully functional

--- packages/server/routes/index.js
+++ packages/server/routes/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import authRouter from './auth'
 import userRouter from './users'
 import postRouter from './posts'
+import activitiesRouter from './activities'
 
 const router = express.Router()
 
@@ -12,5 +13,6 @@ router.get('/', (req, res, next) => {
 router.use('/auth', authRouter)
 router.use('/users', userRouter)
 router.use('/posts', postRouter)
+router.use('/activities', activitiesRouter)
 
 module.exports = router
