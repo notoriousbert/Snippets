ed41b861bb51a862af6c53b65188f122ce918f74
Albert Meuse <albertmeuse22@gmail.com>
2021-11-02 13:07:34 -0700

    added my own personal database for snippets

--- packages/server/config/keys.js
+++ packages/server/config/keys.js
@@ -1,13 +1,14 @@
 module.exports = {
   app: {
-    name: 'Mern Social Media',
-    apiEndpoint: (process.env.API_URL) ? `/${process.env.API_URL}` : '/api',
+    name: "Mern Social Media",
+    apiEndpoint: process.env.API_URL ? `/${process.env.API_URL}` : "/api",
   },
   database: {
-    url: process.env.MONGODB_URI || 'mongodb://localhost:27017/snippets',
+    // url: process.env.MONGODB_URI || 'mongodb://localhost:27017/snippets',
+    url: "mongodb+srv://notoriousbert:BertBert220!!@cluster0.gqhem.mongodb.net/snippets?retryWrites=true&w=majority",
   },
   jwt: {
-    secret: process.env.JWT_SECRET || 'jwt-secret',
-    tokenLife: '7d',
+    secret: process.env.JWT_SECRET || "jwt-secret",
+    tokenLife: "7d",
   },
-}
+};
